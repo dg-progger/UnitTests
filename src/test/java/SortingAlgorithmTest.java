3ad94fa16a1a88a11e90b4bf633bac5ae6143462
Daria Guskova <dasha.gusckowa@gmail.com>
2021-03-02 00:01:36 +0500

    2nd Commit

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -21,61 +21,6 @@ public class SortingAlgorithmTest {
         Assertions.assertArrayEquals(expectedArray, j);
     }
 
-    @Test
-    public void ifCharsTest1() {
-        int[] j = {5, 7, 0, '0', 4};
-        int[] expectedArray = {0, 4, 5, 7, '0'};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
-
-    @Test
-    public void ifCharsTest2() {
-        int[] j = {'1', 1, 0};
-        int[] expectedArray = {0, 1, '1'};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
-
-    @Test
-    public void ifAllCharsTest1() {
-        int[] j = {'5','+','3','=', '8'};
-        int[] expectedArray = {'+','3','5','8','='};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
-
-    @Test
-    public void ifAllCharsTest2() {
-        int[] j = {'5','+','3','=', '8'};
-        int[] expectedArray = {43,51,53,56,61};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
-
-    @Test
-    public void binaryToIntTest() {
-        int[] j = {Integer.parseInt("00101100", 2), Integer.parseInt("10000", 2), Integer.parseInt("0101", 2), Integer.parseInt("0111", 2), Integer.parseInt("1100", 2)};
-        int[] expectedArray = {5, 7, 12, 16, 44};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
-
-    @Test
-    public void intToBinaryTest() {
-        int[] j = {12, 44, 5, 16, 7};
-        int[] expectedArray = {Integer.parseInt("0101", 2), Integer.parseInt("0111", 2), Integer.parseInt("1100", 2), Integer.parseInt("10000", 2), Integer.parseInt("00101100", 2)};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
-
-    @Test
-    public void fractionalNumbersTest() {
-        int[] j = {60/4, 49/7, 36/3, 12/4};
-        int[] expectedArray = {3, 7, 12, 15};
-        SortingAlgorithm.sort(j);
-        Assertions.assertArrayEquals(expectedArray, j);
-    }
 
     @Test
     public void zerosTest() {
@@ -86,17 +31,33 @@ public class SortingAlgorithmTest {
     }
 
     @Test
-    public void stringToIntTest() {
-        int[] j = {Integer.parseInt("24"), Integer.parseInt("17"),Integer.parseInt("45"),Integer.parseInt("103"),Integer.parseInt("2"),};
-        int[] expectedArray = {2, 17, 24, 45, 103};
+    public void ifNegativeNumbersTest() {
+        int[] j = {-5, 7, 0, 14, -97};
+        int[] expectedArray = {-97, -5, 0, 7, 14};
         SortingAlgorithm.sort(j);
         Assertions.assertArrayEquals(expectedArray, j);
     }
 
     @Test
-    public void intToStringTest() {
-        int[] j = {24, 17, 45, 103, 2};
-        int[] expectedArray = {Integer.parseInt("2"), Integer.parseInt("17"),Integer.parseInt("24"),Integer.parseInt("45"),Integer.parseInt("103"),};
+    public void ifBorderNumbersTest() {
+        int[] j = {-2147483648, 2147483647, 0, -2147483648, 3};
+        int[] expectedArray = {-2147483648, -2147483648, 0, 3, 2147483647};
+        SortingAlgorithm.sort(j);
+        Assertions.assertArrayEquals(expectedArray, j);
+    }
+
+    @Test
+    public void ifAlreadySortedNumbersTest() {
+        int[] j = {-2, -1, 0, 0, 1, 2, 3, 4, 5};
+        int[] expectedArray = {-2, -1, 0, 0, 1, 2, 3, 4, 5};
+        SortingAlgorithm.sort(j);
+        Assertions.assertArrayEquals(expectedArray, j);
+    }
+
+    @Test
+    public void ifEmptyArrayTest() {
+        int[] j = {};
+        int[] expectedArray = {};
         SortingAlgorithm.sort(j);
         Assertions.assertArrayEquals(expectedArray, j);
     }
